5425543de681092873996ebfd0ec17c4b2a5f786
david <david@debian.(none)>
2012-09-07 18:13:13 -0400

    updated to include problem 1

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,19 @@
-#include <iostreame>
+ZZZZ#include <iostreame>
 
 using std::cout;
 using std::endl;
+int euler(int n);
+
+int euler(int n) {
+	int sum = 0
+	for (int i = 0; i < n; i++) {
+		if (i%3==0 || i%5==0) {
+			sum+=i;
+}
+}
+}
 int main() {
 	cout<<"hello world"<<endl;
-
+	cout<<euler(1000)<<endl;
 	return 0;
 }
